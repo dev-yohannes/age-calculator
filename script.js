259b57fd98a21db203f4c887b666e5fe57c50565
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-21 21:15:51 +0300

    trying to work on js date object but no luck

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let nameInput = document.querySelector("#name-input");
 let yearInput = document.querySelector("#year-input");
-let monthInput = document.querySelector("#month-input");
-let dayInput = document.querySelector("#day-input");
+// let monthInput = document.querySelector("#month-input");
+// let dayInput = document.querySelector("#day-input");
 let calculateButton = document.querySelector("#calculate-age-button");
 let finalResult = document.querySelector("#result");
 
+// ------------
+// let date = new Date();
+// let yr = date.getFullYear();
+// let month = date.getMonth();
+
+// yearInput.value =
+//   date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+
+// //
+// yearInput.addEventListener("keyup", (e) => {
+//   const [year, month, day] = e.target.value.split("-");
+
+//   console.log({ year, month, day });
+// });
+
+// old one
 function calculatingYear() {
   calculatedYearResult = 2023 - yearInput.value;
   return calculatedYearResult;
@@ -24,11 +40,11 @@ function displayingWithName() {
 
 calculateButton.addEventListener("click", function () {
   finalResult.innerHTML = displayingWithName();
+  console.log(yearInput.value);
 });
 
 document.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
-    // finalResult.innerHTML = calculating();
     finalResult.innerHTML = displayingWithName();
   }
 });
