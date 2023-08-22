c0dd8d6dfa4b8a34f1cb5571b7d68ff421c3e359
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-22 18:43:58 +0300

    adding variables to the signs and the months

--- index.js
+++ index.js
@@ -6,44 +6,53 @@ let dayInput = document.querySelector("#day-input");
 let calculateButton = document.querySelector("#calculate-age-button");
 let finalResult = document.querySelector("#result");
 
+const aquarius = "Aquarius";
+const pieses = "Pieses";
+const aries = "Aries";
+const taurus = "Taurus";
+const gemini = "Gemini";
+const cancer = "Cancer";
+const leo = "Leo";
+const virgo = "Virgo";
+const libra = "Libra";
+const scorpio = "Scorpio";
+const sagittarius = "Sagittarius";
+const capricorn = "Capricorn";
+
+const jan = 0;
+const feb = 1;
+const mar = 2;
+const apr = 3;
+const may = 4;
+const jun = 5;
+const jul = 6;
+const aug = 7;
+const sep = 8;
+const oct = 9;
+const nov = 10;
+const dec = 11;
+
 function calculatingYearInputs() {
   date = new Date(dateInput.value);
   let userYear = date.getFullYear();
   let userMonth = date.getMonth();
   let userDayOfMonth = date.getDate();
 
-  if (userMonth === 0 && userDayOfMonth > 20) {
-    finalResult.innerHTML = "Aquarius";
-  } else if (userMonth === 1 && userDayOfMonth < 18) {
-    finalResult.innerHTML = "Aquarius";
-  } else if (userMonth === 1 && userDayOfMonth > 19) {
-    finalResult.innerHTML = "Pieses";
-  } else if (userMonth === 2 && userDayOfMonth < 20) {
-    finalResult.innerHTML = "Pieses";
-  } else if (userMonth === 2 && userDayOfMonth > 21) {
-    finalResult.innerHTML = "Aries";
-  } else if (userMonth === 3 && userDayOfMonth < 21) {
-    finalResult.innerHTML = "Aries";
+  if (userMonth === jan && userDayOfMonth > 20) {
+    finalResult.innerHTML = `Your Zodiac sign is ${aquarius}`;
+  } else if (userMonth === feb && userDayOfMonth < 18) {
+    finalResult.innerHTML = `Your Zodiac sign is ${aquarius}`;
+  } else if (userMonth === feb && userDayOfMonth > 19) {
+    finalResult.innerHTML = `Your Zodiac sign is ${pieses}`;
+  } else if (userMonth === mar && userDayOfMonth < 20) {
+    finalResult.innerHTML = `Your Zodiac sign is ${pieses}`;
+  } else if (userMonth === mar && userDayOfMonth > 21) {
+    finalResult.innerHTML = `Your Zodiac sign is ${aries}`;
+  } else if (userMonth === apr && userDayOfMonth < 21) {
+    finalResult.innerHTML = `Your Zodiac sign is ${aries}`;
   }
 }
 
 calculateButton.addEventListener("click", function () {
   calculatingYearInputs();
 });
-
-/*
-format = mm/dd/yyy
-
-aquarius (< jan 20 - > feb 18) =
-pieses (< feb 19 - > mar 20) =
-Aries (< mar 21 - > apr 19) =
-taurus (< apr 20 - > may 20)
-gemini (< may 21 - > jun 20)
-cancer (< jun 21 - > jul 22) 
-leo (< jul 23 - > aug 22)
-virgo (< agu 23 - > sep 22)
-libra (< sep 23 - > oct 22)
-scorpio (< oct 23 - > nov 21)
-sagittarius (< nov 22 - > dec 21)
-capricorn (< dec 22 - > jan 19)
-*/
