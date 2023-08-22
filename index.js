90dbe0a1511d973a66c8ac4e778e890a6368cdf6
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-22 21:32:45 +0300

    adding a padding on the name input and finishing up with the else if conditional

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 let nameInput = document.querySelector("#name-input");
 let dateInput = document.querySelector("#date-input");
-let yearInput = document.querySelector("#year-input");
-let monthInput = document.querySelector("#month-input");
-let dayInput = document.querySelector("#day-input");
 let calculateButton = document.querySelector("#calculate-age-button");
 let finalResult = document.querySelector("#result");
 
@@ -32,27 +29,106 @@ const oct = 9;
 const nov = 10;
 const dec = 11;
 
-function calculatingYearInputs() {
-  date = new Date(dateInput.value);
-  let userYear = date.getFullYear();
-  let userMonth = date.getMonth();
-  let userDayOfMonth = date.getDate();
+const finalText = "And your Zodiac sign is";
 
-  if (userMonth === jan && userDayOfMonth > 20) {
-    finalResult.innerHTML = `Your Zodiac sign is ${aquarius}`;
-  } else if (userMonth === feb && userDayOfMonth < 18) {
-    finalResult.innerHTML = `Your Zodiac sign is ${aquarius}`;
-  } else if (userMonth === feb && userDayOfMonth > 19) {
-    finalResult.innerHTML = `Your Zodiac sign is ${pieses}`;
-  } else if (userMonth === mar && userDayOfMonth < 20) {
-    finalResult.innerHTML = `Your Zodiac sign is ${pieses}`;
-  } else if (userMonth === mar && userDayOfMonth > 21) {
-    finalResult.innerHTML = `Your Zodiac sign is ${aries}`;
-  } else if (userMonth === apr && userDayOfMonth < 21) {
-    finalResult.innerHTML = `Your Zodiac sign is ${aries}`;
+function capitalizingFirstLetter() {
+  let strign = nameInput.value;
+  let result = strign.charAt(0).toUpperCase() + strign.slice(1);
+
+  return result;
+}
+
+// const finalGreetingText = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText}`;
+
+function calculatingYearInputs() {
+  date = new Date();
+  userDate = new Date(dateInput.value);
+  let userYear = userDate.getFullYear();
+  let userMonth = userDate.getMonth();
+  let userDayOfMonth = userDate.getDate();
+
+  function calculatingAge() {
+    let calculating = date.getFullYear() - userDate.getFullYear();
+    return calculating;
+  }
+
+  if (
+    (userMonth === jan && userDayOfMonth > 20) ||
+    (userMonth === feb && userDayOfMonth < 18)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${aquarius}`;
+  } else if (
+    (userMonth === feb && userDayOfMonth > 19) ||
+    (userMonth === mar && userDayOfMonth < 20)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${pieses}`;
+  } else if (
+    (userMonth === mar && userDayOfMonth > 21) ||
+    (userMonth === apr && userDayOfMonth < 19)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${aries}`;
+  } else if (
+    (userMonth === apr && userDayOfMonth > 20) ||
+    (userMonth === may && userDayOfMonth < 20)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${taurus}`;
+  } else if (
+    (userMonth === may && userDayOfMonth > 21) ||
+    (userMonth === jun && userDayOfMonth < 20)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${gemini}`;
+  } else if (
+    (userMonth === jun && userDayOfMonth > 21) ||
+    (userMonth === jul && userDayOfMonth < 22)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${cancer}`;
+  } else if (
+    (userMonth === jul && userDayOfMonth > 23) ||
+    (userMonth === aug && userDayOfMonth < 22)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${leo}`;
+  } else if (
+    (userMonth === aug && userDayOfMonth > 23) ||
+    (userMonth === sep && userDayOfMonth < 22)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${virgo}`;
+  } else if (
+    (userMonth === sep && userDayOfMonth > 23) ||
+    (userMonth === oct && userDayOfMonth < 22)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${libra}`;
+  } else if (
+    (userMonth === oct && userDayOfMonth > 23) ||
+    (userMonth === nov && userDayOfMonth < 21)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${scorpio}`;
+  } else if (
+    (userMonth === nov && userDayOfMonth > 22) ||
+    (userMonth === dec && userDayOfMonth < 21)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${sagittarius}`;
+  } else if (
+    (userMonth === dec && userDayOfMonth > 22) ||
+    (userMonth === jan && userDayOfMonth < 19)
+  ) {
+    finalResult.innerHTML = `Hello ${capitalizingFirstLetter()}, You are ${calculatingAge()} years old. ${finalText} ${capricorn}`;
   }
 }
 
 calculateButton.addEventListener("click", function () {
-  calculatingYearInputs();
+  if (nameInput.value === "" && dateInput.value === "") {
+    alert("You need to fill the inputs");
+  } else {
+    calculatingYearInputs();
+  }
+});
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    if (nameInput.value === "" && dateInput.value === "") {
+      alert("You need to fill the inputs");
+    } else {
+      calculatingYearInputs();
+    }
+  }
 });
