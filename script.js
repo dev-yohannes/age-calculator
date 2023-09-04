94590d3cf1fd3fe6aadadbf1f66c246e2132fc1c
Yohannes Mulu <dev.yohannes@gmail.com>
2023-09-04 21:13:21 +0300

    commenting out month variables and setting up dates

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let dateInput = document.querySelector("#date-input");
 let calculateButton = document.querySelector("#calculate-age-button");
 let finalResult = document.querySelector("#result");
 
+/*
 const aquarius = "Aquarius";
 const pieses = "Pieses";
 const aries = "Aries";
@@ -15,23 +16,30 @@ const libra = "Libra";
 const scorpio = "Scorpio";
 const sagittarius = "Sagittarius";
 const capricorn = "Capricorn";
-
-// const jan = 0;
-// const feb = 1;
-// const mar = 2;
-// const apr = 3;
-// const may = 4;
-// const jun = 5;
-// const jul = 6;
-// const aug = 7;
-// const sep = 8;
-// const oct = 9;
-// const nov = 10;
-// const dec = 11;
+*/
+/*
+const jan = 0;
+const feb = 1;
+const mar = 2;
+const apr = 3;
+const may = 4;
+const jun = 5;
+const jul = 6;
+const aug = 7;
+const sep = 8;
+const oct = 9;
+const nov = 10;
+const dec = 11;
+*/
 
 date = new Date();
 userDate = new Date(dateInput.value);
+const userYear = userDate.getFullYear();
+const userMonth = userDate.getMonth();
+const userDayOfMonth = userDate.getDate();
 
-function calculateAge(userDate) {
-  // if userDate.get
-}
+const calculateYear = date.getFullYear() - userYear;
+
+calculateButton.addEventListener("click", function () {
+  console.log(userMonth);
+});
